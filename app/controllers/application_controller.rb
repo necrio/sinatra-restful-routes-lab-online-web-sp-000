3ae8e5c2c610edbf292a8f2c7dbb833d72ae9b26
Keith Isham <kisham8899@gmail.com>
2019-09-14 16:50:06 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,7 @@ class ApplicationController < Sinatra::Base
   end 
   
   get '/recipes/:id/edit' do 
-    @recipes = Recipe.find(params[:id])
+    @recipes = Recipe.find(params[:id].to_i)
+    
 
 end
