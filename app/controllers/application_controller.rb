f1b44288d58d65f57a9e5994e00cff7d8cddaf1a
Keith Isham <kisham8899@gmail.com>
2019-09-14 17:06:52 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,55 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  get '/' do
+    redirect to '/recipes'
+  end
 
-  # code actions here!
+  get '/recipes' do
+    @recipes=Recipe.all
+    erb :index
+  end
 
+  get '/recipes/new' do
+    erb :new
+  end
+
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id].to_i)
+    erb :edit
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id].to_i)
+    erb :show
+  end
+
+  post '/recipes' do
+    recipe = Recipe.new({name: params[:name], 
+                        ingredients: params[:ingredients], 
+                        cook_time: params[:cook_time]})
+    recipe.save
+    redirect to '/recipes/'+recipe.id.to_s
+  end
+
+  patch '/recipes/:id' do
+    recipe = Recipe.find(params[:id].to_i)
+    recipe.update(name: params[:name])
+    recipe.update(ingredients: params[:ingredients])
+    recipe.update(cook_time: params[:cook_time])
+    recipe.save
+    redirect to '/recipes/'+recipe.id.to_s
+  end  
+
+  delete '/recipes/:id/delete' do
+    if (@recipe = Recipe.find_by(id: params[:id])) 
+      @recipe.delete
+      redirect '/recipes'
+    else
+      redirect '/failure'
+    end
+  end
+  
+  
 end
