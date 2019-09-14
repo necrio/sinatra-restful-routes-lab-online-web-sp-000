9838abc47c905df4d0bff75307bd80e273c913fe
Keith Isham <kisham8899@gmail.com>
2019-09-14 16:54:41 +0000

    Automatically backed up by Learn

--- db/migrate/20190914165421_create_recipes.rb
+++ db/migrate/20190914165421_create_recipes.rb
@@ -1,4 +1,10 @@
 class CreateRecipes < ActiveRecord::Migration
-  def change
+   def change
+    create_table :recipes do |t|
+      t.string :name
+      t.string :ingredients
+      t.string :cook_time
+    end
   end
+
 end
